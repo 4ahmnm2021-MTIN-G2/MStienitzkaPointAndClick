5542355b28847c1021ac65c831b92edfb309e55c
max <max.stienitzka@gmail.com>
2021-03-15 14:57:33 +0100

    moving script updated/reramped with DoTween Plugin and now fully fucional

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using UnityEngine;
+using DG.Tweening;
+
+public class MoveScript : MonoBehaviour
+{
+ public Camera cam;
+ public GameObject Cube;
+ public float time;
+
+    void Update()
+    {
+      if (Input.GetMouseButtonDown(0))  {
+
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+            Cube.transform.DOMove(hit.point, 0.5f);
+            Debug.Log("LOLOL");
+            }
+        
+      }
+      
+    }
+}
