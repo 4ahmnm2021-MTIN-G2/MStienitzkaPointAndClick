37879a39ea1ac0c13e3ff811b63f58fe82a17a6d
max <max.stienitzka@gmail.com>
2021-05-09 19:28:45 +0200

    added objects, updated scripts

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,36 +1,30 @@
-﻿using System;
-using System.Net;
+﻿using DG.Tweening;
 using UnityEngine;
-using DG.Tweening;
 
-public class MoveScript : MonoBehaviour
-{
- public Camera cam;
- public GameObject Cube;
- public float time;
- public GameObject Canvas;
- public Collider coll;
+public class MoveScript : MonoBehaviour {
+  public Camera cam;
+  public GameObject cube;
+  public float time;
+  public GameObject Canvas;
+  public Collider coll;
+  public float oldPos;
+  public float newPos;
+  public float velocity;
 
-    void Update()
-    {
-      if (Input.GetMouseButtonDown(0))  {
+  void Update () {
+    if (Input.GetMouseButtonDown (0)) {
 
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (coll.Raycast(ray, out hit, 100.0f))
-            {
-            Cube.transform.DOMove(hit.point, time);
-            Debug.Log("Move");
-            }
-        
+      Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+      RaycastHit hit;
+      oldPos = cube.transform.position.magnitude;
+      if (coll.Raycast (ray, out hit, 100.0f)) {
+        cube.transform.DOMove (hit.point, time);
+        Debug.Log ("Move");
       }
     }
-void OnTriggerEnter(Collider target)
-{
-  if(target.tag == "Interactable"){
-    Canvas.SetActive(true );
-    Debug.Log("trigger");
+    if (Input.GetMouseButtonUp (0)) {
+      newPos = cube.transform.position.magnitude;
+      velocity = oldPos - newPos / time;
+    }
   }
 }
-}
-
--- Assets/Scripts/onTrigger.cs
+++ Assets/Scripts/onTrigger.cs
@@ -1,66 +1,73 @@
-﻿using System.Net.Mime;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Net.Mime;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class onTrigger : MonoBehaviour
-{
-public GameObject can;
-public Text objectText;
-public string cylinderout;
-public string ballout;
-private bool ball = false;
-private bool balltext = false;
-private bool cylindertext = false;
-private bool cyl = false;
+public class onTrigger : MonoBehaviour {
+  public GameObject can;
+  public GameObject Zeitung;
+  public Text objectText;
+  public string cylinderout;
+  public string ballout;
+  public string binInteractText;
+  private bool ball = false;
+  private bool balltext = false;
+  private bool cylindertext = false;
+  private bool cyl = false;
 
-//Check Collider
-void OnTriggerEnter(Collider target)
-{
-  if(target.tag == "Interactable"){
-    can.SetActive(true );
-    ball = true;
-    if (balltext == true){
-    objectText.text = ballout;
-    Debug.Log("trigger enter");
-   
-    }
-  }
-   if(target.tag == "Cylinder"){
-    can.SetActive(true );
-    cyl = true;
-    if (cylindertext == true){
-    objectText.text = cylinderout;
-    }
-    Debug.Log("cylinder exit");
-  }
-}
-void OnTriggerExit(Collider target)
-{
-  if(target.tag == "Interactable"){
-    can.SetActive(false );
-    ball = false;
-    Debug.Log("trigger exit");
-    objectText.text = "???";
-  }
-     if(target.tag == "Cylinder"){
-    can.SetActive(false );
-    cyl = false;
-    Debug.Log("cylinder exit");
-    objectText.text = "???";
-}    
-}
-
-public void Inspect(){
-    if(ball == true){
-
+  //Check Collider
+  void OnTriggerEnter (Collider target) {
+    if (target.tag == "Interactable") {
+      can.SetActive (true);
+      ball = true;
+      if (balltext == true) {
         objectText.text = ballout;
-        balltext = true;
+        Debug.Log ("trigger enter");
+
+      }
     }
-    if(cyl == true){
+    if (target.tag == "Cylinder") {
+      can.SetActive (true);
+      cyl = true;
+      if (cylindertext == true) {
         objectText.text = cylinderout;
-        cylindertext = true;
-}
-}
+      }
+      Debug.Log ("cylinder exit");
+    }
+  }
+  void OnTriggerExit (Collider target) {
+    if (target.tag == "Interactable") {
+      can.SetActive (false);
+      ball = false;
+      Debug.Log ("trigger exit");
+      objectText.text = "???";
+    }
+    if (target.tag == "Cylinder") {
+      can.SetActive (false);
+      cyl = false;
+      Debug.Log ("cylinder exit");
+      objectText.text = "???";
+    }
+  }
+
+  public void Inspect () {
+    if (ball == true) {
+
+      objectText.text = ballout;
+      balltext = true;
+    }
+    if (cyl == true) {
+      objectText.text = cylinderout;
+      cylindertext = true;
+    }
+  }
+
+  public void Interact () {
+    if (ball && balltext) {
+      ballout = binInteractText;
+      objectText.text = binInteractText;
+      Zeitung.SetActive (false);
+    }
+  }
 }
